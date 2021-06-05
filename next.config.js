f4d5105126a9e67a2d6f5c823ca2388cf84b8102
Lautaro Valentini <lautiivalentini@gmail.com>
2021-06-05 12:12:43 -0300

    delete console.log

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
         webpack5: true
     },
     webpack: function (config, options) {
-        console.log(options.webpack.version); // 5.18.0
         config.experiments = {};
         return config;
     }
