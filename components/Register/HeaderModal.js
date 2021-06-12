0befd155a3f2ee6ae319251284d212499b4782ef
Lautaro Valentini <lautivalentini19@gmail.com>
2021-06-12 00:02:39 -0300

    add_steps_register

---
+++ components/Register/HeaderModal.js
@@ -0,0 +1,72 @@
+import {
+  Button,
+  Box,
+  Text
+} from "@chakra-ui/react"
+import { IoLogoTwitter, IoMdClose } from 'react-icons/io'
+import { useContext } from "react"
+
+import RegisterContext from "../../context/RegisterContext";
+
+const HeaderModal = ({ updateStep, steps }) => {
+  const { registerData } = useContext(RegisterContext);
+  return (
+    <>
+      {steps > 1 && (
+      <Box 
+        width="22.5px" 
+        height="22.5px" 
+        fontSize="22.5px" 
+        color="_blue"
+        position="absolute"
+        top="15px"
+        left="10px"
+        cursor="pointer"
+        onClick={() => updateStep("decrement")}
+      >
+        <IoMdClose />
+      </Box>
+      )}
+      {steps > 2 ? (
+        <Text ml={10} fontSize="20px" color="_white">Paso {steps} de 5</Text>
+      ) : (
+        <>
+        <Box 
+          width="30px" 
+          height="30px" 
+          fontSize="30px" 
+          margin="auto" 
+          color="_white"
+        >
+          <IoLogoTwitter />
+        </Box>
+        <Button 
+          size="sm" 
+          bg="_blue" 
+          color="white"
+          position="absolute"
+          top="10px"
+          right="10px"
+          fontSize="15px"
+          borderRadius={9999}
+          disabled={
+            registerData.phone === '' ||
+            registerData.name === '' ||
+            registerData.month === '' ||
+            registerData.day === '' ||
+            registerData.year === '' 
+            ? true : false
+          }
+          _hover={{bg: "#1DA1F2", opacity: "0.9"}}
+          onClick={() => updateStep("increment")}
+        >
+          Siguiente
+        </Button>
+      </>
+      )}
+    </>
+  )
+
+}
+
+export default HeaderModal
--- components/Register/index.js
+++ components/Register/index.js
@@ -4,27 +4,32 @@ import {
   ModalContent,
   ModalHeader,
   ModalBody,
-  Text,
-  Button,
-  Box,
-  Stack,
-  Input,
-  Select
 } from "@chakra-ui/react"
 import { useState } from "react"
-import { IoLogoTwitter } from 'react-icons/io'
+
+import FirstStep from "./FirstStep"
+import HeaderModal from "./HeaderModal"
+import SecondStep from "./SecondStep"
+import ThirdStep from "./ThirdStep"
 
 const Register = ({isOpen, onOpen, onClose}) => {
-  const [showText, setShowText] = useState({
-    name: false,
-    phone: false,
-    month: false,
-    day: false,
-    year: false,
-  })
+  const [steps, setSteps] = useState(1)
+
+  function updateStep(action) {
+    if (action === 'decrement') {
+      return setSteps(steps - 1)
+    }
+    if (action === 'increment') {
+      return setSteps(steps + 1)
+    }
+    if (!isNaN(action)) {
+      return setSteps(action)
+    }
+  }
+
   return (
       <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false} size="xl">
-        <ModalOverlay bg="#242d34" />
+        <ModalOverlay bg="_overlay" />
         <ModalContent 
           bg="_black" 
           height={{base: '100vh', md: '600px'}}
@@ -32,131 +37,12 @@ const Register = ({isOpen, onOpen, onClose}) => {
           margin="auto 0"
         >
           <ModalHeader padding="10px">
-            <Box 
-              width="30px" 
-              height="30px" 
-              fontSize="30px" 
-              margin="auto" 
-              color="_white"
-            >
-            <IoLogoTwitter />
-            </Box>
-            <Button 
-              size="sm" 
-              bg="_blue" 
-              color="white"
-              position="absolute"
-              top="10px"
-              right="10px"
-              fontSize="15px"
-              borderRadius={9999}
-            >
-            Siguiente
-            </Button>
+            <HeaderModal steps={steps} updateStep={updateStep} />
           </ModalHeader>
           <ModalBody>
-            <Stack spacing={7}>
-              <Text color="white" fontWeight="bold" fontSize="23px">Crea tu cuenta</Text>
-                <Box position="relative" width="100%">
-                  <Input 
-                    color="_white" 
-                    borderColor="_gray" padding={showText.name ? "40px 0 20px 10px" : "28px 10px"} 
-                    placeholder={!showText.name ? "Nombre" : null} 
-                    width="100%" 
-                    maxWidth="550px" 
-                    sx={{ "::placeholder": { color: "#6e767d" }  }}
-                    onFocus={() => setShowText({...showText, name: true})}
-                    onBlur={(e) => {
-                      if (e.target.value === '') {
-                        setShowText({...showText, name: false})
-                      }
-                    }}
-                  />
-                  {showText.name && <Text fontSize="12px" color="_blue" position="absolute" top="10px" left="10px">Nombre</Text>}
-                </Box>
-                <Box position="relative" width="100%">
-                  <Input 
-                    color="_white" 
-                    borderColor="_gray" 
-                    padding={showText.phone ? "40px 0 20px 10px" : "28px 10px"} 
-                    placeholder={!showText.phone ? "Teléfono" : null} 
-                    width="100%" 
-                    maxWidth="550px" 
-                    sx={{ "::placeholder": { color: "#6e767d" }  }}
-                    onFocus={() => setShowText({...showText, phone: true})}
-                    onBlur={(e) => {
-                      if (e.target.value === '') {
-                        setShowText({...showText, phone: false})
-                      }
-                    }}
-                  />
-                    {showText.phone && <Text fontSize="12px" color="_blue" position="absolute" top="10px" left="10px">Télefono</Text>}
-                </Box>
-             <Stack spacing={3}>
-             <Stack spacing={0} fontSize="15px">
-                <Text color="white" fontWeight="bold">Fecha de nacimiento</Text>
-                <Text color="_textInput" textAlign="justify">
-                Esta información no será pública. Confirma tu propia edad, incluso si esta cuenta es para una empresa, una mascota u otra cosa.
-                </Text>
-              </Stack>
-              <Stack direction={{base: 'column', md: 'row'}} spacing={3}>
-                <Box width={{base: '100%', md: '50%'}} position="relative" >
-                  <Select 
-                    color="white"
-                    fontSize="14px" 
-                    borderColor="_gray" 
-                    width="100%" 
-                    height="55px"
-                    onFocus={() => setShowText({...showText, month: true})}
-                    onBlur={() => setShowText({...showText, month: false})}
-                    style={{ paddingTop: '20px', paddingLeft: '10px' }}
-                  >
-                    <option value=""></option>
-                    <option paddingTop="20px" value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                  </Select>
-                  <Text fontSize="12px" color={showText.month ? '_blue' : '_textInput'} position="absolute" top="10px" left="10px">Mes</Text>
-                </Box>
-                <Box width={{base: '100%', md: '25%'}} position="relative" >
-                  <Select 
-                    color="white"
-                    fontSize="14px" 
-                    borderColor="_gray" 
-                    width="100%" 
-                    height="55px"
-                    onFocus={() => setShowText({...showText, day: true})}
-                    onBlur={() => setShowText({...showText, day: false})}
-                    style={{ paddingTop: '20px', paddingLeft: '10px' }}
-                  >
-                    <option value=""></option>
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                  </Select>
-                  <Text fontSize="12px" color={showText.day ? '_blue' : '_textInput'} position="absolute" top="10px" left="10px">Día</Text>
-                </Box>
-                <Box width={{base: '100%', md: '25%'}} position="relative" >
-                  <Select 
-                    color="white"
-                    fontSize="14px" 
-                    borderColor="_gray" 
-                    width="100%" 
-                    height="55px"
-                    onFocus={() => setShowText({...showText, year: true})}
-                    onBlur={() => setShowText({...showText, year: false})}
-                    style={{ paddingTop: '20px', paddingLeft: '10px' }}
-                  >
-                    <option value=""></option>
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                  </Select>
-                  <Text fontSize="12px" color={showText.year ? '_blue' : '_textInput'} position="absolute" top="10px" left="10px">Año</Text>
-                </Box>
-              </Stack>
-             </Stack>
-            </Stack>
+            {steps === 1 && <FirstStep />}
+            {steps === 2 && <SecondStep />}
+            {steps === 3 && <ThirdStep updateStep={updateStep} />}
           </ModalBody>
         </ModalContent>
       </Modal>
